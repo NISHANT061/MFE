a2ec7d53572a6d588b38fb6c43d0cbfdc62e5e7b
Nishant Singh <cmritnishant@gmail.com>
2025-04-02 23:55:11 +0530

    wtp

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,13 +2,4 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App';
 
-const mount =(el)=>{
-    ReactDom.render(<App/>,el)
-};
-
-if ( process.env.NODE_ENV === 'development'){
-    const devRoot = document.querySelector("#root")
-    if (devRoot)
-    mount(devRoot)
-}
-module.exports =  mount;
+ReactDom.render(<App/>,document.querySelector("#root"))
