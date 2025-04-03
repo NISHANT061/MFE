0af27b76677909bd921a56fad8c74c4c9a2765a8
Nishant kumar <nishant.2.kumar@bt.com>
2025-04-03 08:19:45 +0530

    fixing css issues

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import  {BrowserRouter}  from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import MakrketingApp from "./components/marketingApp";
 import Header from "./components/Header";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 export default () => {
-  return (<BrowserRouter>
-    <div>
-      <Header />
-      <MakrketingApp />
-    </div>
-  </BrowserRouter>);
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MakrketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
+  );
 };
